7e1da58967cb0aeecebe0feb9d9654ac2d3e5ff7
Nicklaus Liow <nicklaus.liow@gmail.com>
2013-12-12 00:18:05 -0800

    directional conveyor art

--- code/demo7 - conveyor/game/scripts/src/level/Conveyor.js
+++ code/demo7 - conveyor/game/scripts/src/level/Conveyor.js
@@ -18,27 +18,28 @@
 		this.direction = config.direction;
 		this.activator = config.activator;
 
+		this.active = false;
 		this.update = function(){
 
 			// Am I active?
-			var active = false;
+			self.active = false;
 			var myPrismID = self.activator;
 			if(myPrismID==""){
-				active = true;
+				self.active = true;
 			}else{
 				var myPrism = level.prisms.map[myPrismID];
 				if(myPrism && myPrism.active && myPrism.seesHuman){
-					active = true;
+					self.active = true;
 				}
 			}
 
 			// If any of the props are on me, and I'm active, move 'em!
-			if(active){
+			if(self.active){
 				var props = [level.player].concat(level.prisms.prisms).concat(level.dummies.dummies);
 				for(var i=0;i<props.length;i++){
 					var prop = props[i];
 
-					if(self.belt.ax<prop.x && self.belt.bx>prop.x && self.belt.ay<prop.y && self.belt.by>prop.y){
+					if(self.belt.ax+5<prop.x && self.belt.bx-5>prop.x && self.belt.ay+5<prop.y && self.belt.by-5>prop.y){
 						prop.x += self.direction.x;
 						prop.y += self.direction.y;
 					}
@@ -48,14 +49,27 @@
 		};
 
 		var pattern = Display.context.tmp.createPattern(Asset.image.conveyor, 'repeat');
+		var directionAngle = Math.atan2(self.direction.y,self.direction.x);
+		var directionMag = Math.sqrt(self.direction.x*self.direction.x + self.direction.y*self.direction.y);
+		var distance = 0;
 		this.draw = function(){
+
+			if(self.active){
+				distance += directionMag;
+				if(distance>50) distance-=50;
+			}
+
 			var ctx = Display.context.game;
 			ctx.fillStyle = pattern;
 			ctx.beginPath();
 			ctx.rect(self.belt.ax, self.belt.ay, self.belt.bx-self.belt.ax, self.belt.by-self.belt.ay);
-			//ctx.translate(0,cctvY);
+			
+			ctx.save();
+			ctx.rotate(directionAngle);
+			ctx.translate(distance,0);
 			ctx.fill();
-			//ctx.translate(0,-cctvY);
+			ctx.restore();
+
 		};
 
 	};
