08851b8b3d4ab5fc180a1ac074379a5d3553b49c
Nicklaus Liow <nicklaus.liow@gmail.com>
2013-12-14 13:55:47 -0800

    Moveable blocks are now a thing

---
+++ code/demo9 - moveable props/game/scripts/src/level/Conveyor.js
@@ -0,0 +1,89 @@
+(function(exports){
+
+	var Conveyor = function(level,config){
+
+		var self = this;
+		
+		// Properties
+		this.level = level;
+		this.config = config;
+
+		// Config
+		this.belt = config.belt;
+		this.belt.ax *= Map.TILE_SIZE;
+		this.belt.ay *= Map.TILE_SIZE;
+		this.belt.bx *= Map.TILE_SIZE;
+		this.belt.by *= Map.TILE_SIZE;
+
+		this.direction = config.direction;
+		this.activator = config.activator;
+
+		this.active = false;
+		this.update = function(){
+
+			// Am I active?
+			self.active = false;
+			var myPrismID = self.activator;
+			if(myPrismID==""){
+				self.active = true;
+			}else{
+				var myPrism = level.prisms.map[myPrismID];
+				if(myPrism && myPrism.active && myPrism.seesHuman){
+					self.active = true;
+				}
+			}
+
+			// If any of the props are on me, and I'm active, move 'em!
+			if(self.active){
+				var props = [level.player].concat(level.prisms.prisms).concat(level.dummies.dummies).concat(level.blocks.blocks);
+				for(var i=0;i<props.length;i++){
+					var prop = props[i];
+
+					// With a 5px padding so things don't go over the edge
+					//if(self.belt.ax+5<prop.x && self.belt.bx-5>prop.x && self.belt.ay+5<prop.y && self.belt.by-5>prop.y){
+					if(    _isOnBelt(prop.x+5,prop.y)
+						|| _isOnBelt(prop.x+5,prop.y-10)
+						|| _isOnBelt(prop.x-5,prop.y)
+						|| _isOnBelt(prop.x-5,prop.y-10)
+					){
+						prop.x += self.direction.x;
+						prop.y += self.direction.y;
+					}
+				}
+			}
+
+		};
+
+		var _isOnBelt = function(x,y){
+			return(self.belt.ax<=x && self.belt.bx>=x && self.belt.ay<=y && self.belt.by>=y);
+		}
+
+		var pattern = Display.context.tmp.createPattern(Asset.image.conveyor, 'repeat');
+		var directionAngle = Math.atan2(self.direction.y,self.direction.x);
+		var directionMag = Math.sqrt(self.direction.x*self.direction.x + self.direction.y*self.direction.y);
+		var distance = 0;
+		this.draw = function(){
+
+			if(self.active){
+				distance += directionMag;
+				if(distance>50) distance-=50;
+			}
+
+			var ctx = Display.context.game;
+			ctx.fillStyle = pattern;
+			ctx.beginPath();
+			ctx.rect(self.belt.ax, self.belt.ay, self.belt.bx-self.belt.ax, self.belt.by-self.belt.ay);
+			
+			ctx.save();
+			ctx.rotate(directionAngle);
+			ctx.translate(distance,0);
+			ctx.fill();
+			ctx.restore();
+
+		};
+
+	};
+
+	exports.Conveyor = Conveyor;
+
+})(window);
