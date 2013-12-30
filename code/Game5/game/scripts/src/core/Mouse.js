22c4a3375c14f0e54918b7b4cd5f0fef85b48a5b
Nicklaus Liow <nicklaus.liow@gmail.com>
2013-12-30 03:26:11 -0800

    All end cutscene flash and pics

---
+++ code/Game5/game/scripts/src/core/Mouse.js
@@ -0,0 +1,38 @@
+(function(exports){
+
+	// Singleton
+	var Mouse = {
+		x: 0,
+		y: 0,
+		pressed: false
+	};
+	exports.Mouse = Mouse;
+
+	// Event Handling
+	var onMouseDown = function(event){
+	    Mouse.pressed = true;
+	    onMouseMove(event);
+	};
+	var onMouseUp = function(event){
+	    Mouse.pressed = false;
+	};
+	var onMouseMove = function(event){
+		
+		Mouse.realX = event.clientX;
+		Mouse.realY = event.clientY;
+
+		var container = document.querySelector("canvas#game");
+		if(!container){
+			Mouse.x = Mouse.realX;
+			Mouse.y = Mouse.realY;
+			return;
+		}
+		Mouse.x = event.clientX - container.offsetLeft;
+		Mouse.y = event.clientY - container.offsetTop;
+
+	};
+	window.onmousedown = onMouseDown;
+	window.onmouseup = onMouseUp;
+	window.onmousemove = onMouseMove;
+
+})(window);
