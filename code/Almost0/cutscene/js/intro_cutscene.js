9481eb9ac2db5197ebf6db9c4a9c2add381c5bac
Nicklaus Liow <nicklaus.liow@gmail.com>
2014-01-22 20:34:32 -0800

    All audio is in for first cutscene

--- code/Almost0/cutscene/js/intro_cutscene.js
+++ code/Almost0/cutscene/js/intro_cutscene.js
@@ -6,207 +6,8 @@ window.gotoGame = function(){
 	window.top.gotoPage("level");
 }
 
-window.onload = function(){
-
-	var manifest = [
-		{id:"door_open", src:"sounds/door_open.wav"},
-		{id:"door_close", src:"sounds/door_close.wav"},
-		{id:"city", src:"sounds/city.mp3"},
-		{id:"room", src:"sounds/room.mp3"}
-	];
-
-	var soundsLeft = manifest.length;
-	createjs.Sound.addEventListener("fileload",function(event){
-		soundsLeft--;
-		if(soundsLeft==0) onLoad();
-	});
-	createjs.Sound.registerManifest(manifest);
-
-};
 
 var onLoad = function(){
-
-	var poster = {
-		poppy: {
-			user: "Poppy Gardner",
-			icon: "icons/poppy.png"
-		},
-		gardner: {
-			user: "George Gardner",
-			icon: "icons/gardner.png"
-		},
-		game: {
-			user: "Nothing To Hide",
-			icon: "icons/game.png",
-			date: "just now"
-		}
-	};
-
-	var _generatePost = function(type,poster,date,data){
-		return {
-			type: type+"_post",
-			user: poster.user,
-			icon: poster.icon,
-			date: date,
-			data: data
-		}
-	};
-
-	var posts = [
-
-		////// ESTABLISHING SHOT ///////
-
-		_generatePost("parallax",poster.poppy,"16 minutes ago",{
-			height: 450,
-			layers:[
-				{img:"pics/background_back.png", depth:0.8, offset:0},
-				{img:"pics/background_front.png", depth:0.6, offset:0},
-				{img:"pics/establishing_fg.png", depth:0.1, offset:50}
-			]
-		}),
-		_generatePost("parallax",poster.poppy,"16 minutes ago",{
-			height: 350,
-			layers:[
-				{img:"pics/background_back.png", depth:0.8, offset:0},
-				{img:"pics/background_front.png", depth:0.6, offset:0},
-				{img:"pics/establishing_fg_2.png", depth:0.1, offset:50}
-			],
-			sound: {
-				offset: 0,
-				play: ["door_open",null,0,0,0,1,-1]
-			}
-		}),
-		_generatePost("parallax",poster.poppy,"16 minutes ago",{
-			height: 250,
-			layers:[
-				{img:"pics/background_back.png", depth:0.8, offset:0},
-				{img:"pics/background_front.png", depth:0.6, offset:0},
-				{img:"pics/establishing_fg_3.png", depth:0.1, offset:50}
-			]
-		}),
-
-
-		//////// MR GARDNER TO POPPY //////////
-
-		_generatePost("conversation",poster.gardner,"16 minutes ago",{
-			message: "Poppy?"
-		}),
-		_generatePost("conversation",poster.gardner,"16 minutes ago",{
-			message: "Hey sweetie. Please don't be sad."
-		}),
-		_generatePost("conversation",poster.gardner,"16 minutes ago",{
-			message: "It's really hurting my popularity ranking."
-		}),
-		_generatePost("conversation",poster.gardner,"15 minutes ago",{
-			message: "The first un-secret ballot election is coming up. "+
-					"It's very important to Daddy's career, "+
-					"so I need you to post more happy pictures of yourself to The Wall. "+
-					"At least 400 per day, okay?"
-		}),
-		_generatePost("conversation",poster.gardner,"15 minutes ago",{
-			message: "Please, Poppy? I don't want the voters to think I'm a bad parent."
-		}),
-
-
-		///// CLOSE DOOR /////
-
-		_generatePost("parallax",poster.poppy,"15 minutes ago",{
-			height: 450,
-			layers:[
-				{img:"pics/background_back.png", depth:0.8, offset:0},
-				{img:"pics/background_front.png", depth:0.6, offset:0},
-				{img:"pics/door_close_1.png", depth:0.1, offset:50}
-			]
-		}),
-		_generatePost("conversation",poster.gardner,"15 minutes ago",{
-			message: "Well, goodnight! <span>#DaddyDaughterMoment</span>"
-		}),
-		_generatePost("parallax",poster.poppy,"15 minutes ago",{
-			height: 450,
-			layers:[
-				{img:"pics/background_back.png", depth:0.8, offset:0},
-				{img:"pics/background_front.png", depth:0.6, offset:0},
-				{img:"pics/door_close_2.png", depth:0.1, offset:75}
-			]
-		}),
-		_generatePost("parallax",poster.poppy,"14 minutes ago",{
-			height: 450,
-			layers:[
-				{img:"pics/background_back.png", depth:0.8, offset:0},
-				{img:"pics/background_front.png", depth:0.6, offset:0},
-				{img:"pics/door_close_3.png", depth:0.1, offset:100}
-			],
-			sound: {
-				offset: 200,
-				play: ["door_close",null,0,0,0,1,-1]
-			}
-		}),
-		_generatePost("parallax",poster.poppy,"14 minutes ago",{
-			height: 450,
-			layers:[
-				{img:"pics/background_back.png", depth:0.8, offset:0},
-				{img:"pics/background_front.png", depth:0.6, offset:0},
-				{img:"pics/door_close_4.png", depth:0.1, offset:100}
-			]
-		}),
-
-		//////// INTERNAL MONOLOGUE //////////
-
-		_generatePost("conversation",poster.poppy,"14 minutes ago",{
-			message: "I... I have to get out of here..."
-		}),
-		_generatePost("conversation",poster.poppy,"14 minutes ago",{
-			message: "Dad will be better off without me. His popularity metrics, too."
-		}),
-		_generatePost("conversation",poster.poppy,"14 minutes ago",{
-			message: "I don't deserve his loving security and discipline."
-		}),
-		_generatePost("conversation",poster.poppy,"14 minutes ago",{
-			message: "Goodbye, dad. I'm so sorry.",
-			ambience: {
-				interval: [-40001,-40000,-500,500],
-				play: "room",
-				volume: 1
-			}
-		}),
-
-
-		//////// ESCAPE //////////
-
-		_generatePost("parallax",poster.poppy,"8 minutes ago",{
-			height: 250,
-			layers:[{img:"pics/escape_1.png", depth:1, offset:150}],
-			ambience: {
-				interval: [-700,0,1700,2200],
-				play: "city",
-				volume: 1
-			}
-		}),
-		_generatePost("parallax",poster.poppy,"8 minutes ago",{
-			height: 250,
-			layers:[{img:"pics/escape_2.png", depth:1, offset:150}]
-		}),
-		_generatePost("parallax",poster.poppy,"8 minutes ago",{
-			height: 250,
-			layers:[{img:"pics/escape_3.png", depth:1, offset:150}]
-		}),
-		_generatePost("parallax",poster.poppy,"8 minutes ago",{
-			height: 250,
-			layers:[{img:"pics/escape_4.png", depth:1, offset:150}]
-		}),
-		_generatePost("parallax",poster.poppy,"8 minutes ago",{
-			height: 250,
-			layers:[{img:"pics/escape_5.png", depth:1, offset:150}]
-		}),
-
-		//////// PLAY GAME /////////
-		_generatePost("video",poster.game,"just now",{
-			bg: "pics/game.png"
-		})
-
-	];
-
-	///////////
 
 	var timeline = document.getElementById("timeline");
 	var soundEffects = [];
@@ -323,22 +124,14 @@ var onLoad = function(){
 		for(var i=0;i<soundEffects.length;i++){
 			
 			var sfx = soundEffects[i];
-			if(sfx.played) continue; // play just once
 
 			// Play when halfway at screen
-			if( (sfx.dom.offsetTop - window.scrollY) + sfx.sound.offset < window.innerHeight*0.6){
-				sfx.played = true;
-				createjs.Sound.play.apply(null,sfx.sound.play);
-			}
-
-			/*// Play when halfway at screen
 			if( (sfx.dom.offsetTop - window.scrollY) + sfx.sound.offset < window.innerHeight*0.6){
 				if(!sfx.past) createjs.Sound.play.apply(null,sfx.sound.play);
 				sfx.past = true;
 			}else{
-				if(sfx.past) createjs.Sound.play.apply(null,sfx.sound.play);
 				sfx.past = false;
-			}*/
+			}
 
 		}
 
