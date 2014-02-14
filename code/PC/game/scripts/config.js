87773c7280c2311773c9bd2e3b787140ff0721c4
Nicklaus Liow <nicklaus.liow@gmail.com>
2014-02-14 13:34:24 -0800

    PC version works on all

--- code/PC/game/scripts/config.js
+++ code/PC/game/scripts/config.js
@@ -67,29 +67,29 @@ Asset.init({
 	},
 	sounds:{
 
-		sfx_shotdown:{src:'assets/sounds/mp3/shotdown.mp3|assets/sounds/ogg/shotdown.ogg'},
-		sfx_alarm:{src:'assets/sounds/mp3/alarm.mp3|assets/sounds/ogg/alarm.ogg'},
+		sfx_shotdown:{src:'assets/sounds/ogg/shotdown.ogg'},
+		sfx_alarm:{src:'assets/sounds/ogg/alarm.ogg'},
 		
-		sfx_prism_pickup:{src:'assets/sounds/mp3/prism_pickup.mp3|assets/sounds/ogg/prism_pickup.ogg'},
-		sfx_prism_putdown:{src:'assets/sounds/mp3/prism_putdown.mp3|assets/sounds/ogg/prism_putdown.ogg'},
+		sfx_prism_pickup:{src:'assets/sounds/ogg/prism_pickup.ogg'},
+		sfx_prism_putdown:{src:'assets/sounds/ogg/prism_putdown.ogg'},
 
-		sfx_footstep_1:{src:'assets/sounds/mp3/step_1.mp3|assets/sounds/ogg/step_1.ogg'},
-		sfx_footstep_2:{src:'assets/sounds/mp3/step_2.mp3|assets/sounds/ogg/step_2.ogg'},
-		sfx_metal_footstep_1:{src:'assets/sounds/mp3/metal_step_1.mp3|assets/sounds/ogg/metal_step_1.ogg'},
-		sfx_metal_footstep_2:{src:'assets/sounds/mp3/metal_step_2.mp3|assets/sounds/ogg/metal_step_2.ogg'},
+		sfx_footstep_1:{src:'assets/sounds/ogg/step_1.ogg'},
+		sfx_footstep_2:{src:'assets/sounds/ogg/step_2.ogg'},
+		sfx_metal_footstep_1:{src:'assets/sounds/ogg/metal_step_1.ogg'},
+		sfx_metal_footstep_2:{src:'assets/sounds/ogg/metal_step_2.ogg'},
 		
-		sfx_carpet_footstep_1:{src:'assets/sounds/mp3/carpet_step_1.mp3|assets/sounds/ogg/carpet_step_1.ogg'},
-		sfx_carpet_footstep_2:{src:'assets/sounds/mp3/carpet_step_2.mp3|assets/sounds/ogg/carpet_step_2.ogg'},
+		sfx_carpet_footstep_1:{src:'assets/sounds/ogg/carpet_step_1.ogg'},
+		sfx_carpet_footstep_2:{src:'assets/sounds/ogg/carpet_step_2.ogg'},
 
-		sfx_cam:{src:'assets/sounds/mp3/cam.mp3|assets/sounds/ogg/cam.ogg'},
-		sfx_slidewalk:{src:'assets/sounds/mp3/slidewalk.mp3|assets/sounds/ogg/slidewalk.ogg'},
-		sfx_dialog:{src:'assets/sounds/mp3/dialog.mp3|assets/sounds/ogg/dialog.ogg'},
+		sfx_cam:{src:'assets/sounds/ogg/cam.ogg'},
+		sfx_slidewalk:{src:'assets/sounds/ogg/slidewalk.ogg'},
+		sfx_dialog:{src:'assets/sounds/ogg/dialog.ogg'},
 
-		button_hover: {src:'assets/sounds/mp3/button_hover.mp3|assets/sounds/ogg/button_hover.ogg'},
-		button_press: {src:'assets/sounds/mp3/button_press.mp3|assets/sounds/ogg/button_press.ogg'},
+		button_hover: {src:'assets/sounds/ogg/button_hover.ogg'},
+		button_press: {src:'assets/sounds/ogg/button_press.ogg'},
 
-		music_bg: {src:'assets/music/mp3_64/a_healthy_dystopia.mp3|assets/music/ogg_64/a_healthy_dystopia.ogg'},
-		music_bg_2: {src:'assets/music/mp3_64/home_sweet_homeland.mp3|assets/music/ogg_64/home_sweet_homeland.ogg'}
+		music_bg: {src:'assets/music/ogg_64/a_healthy_dystopia.ogg'},
+		music_bg_2: {src:'assets/music/ogg_64/home_sweet_homeland.ogg'}
 
 	},
 	levels:{
